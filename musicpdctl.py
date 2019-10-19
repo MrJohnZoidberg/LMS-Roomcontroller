1b5cce054d0d60f39f74af265dfeb9427000cc07
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-19 15:11:11 +0200

    Update

--- musicpdctl.py
+++ musicpdctl.py
@@ -10,7 +10,7 @@ class MPDControll:
     def list_database(mpd_type):
         process = pexpect.spawnu(f"mpc list {mpd_type}", echo=False, timeout=3)
         process.expect([pexpect.EOF])
-        out = process.before.split("\n")
+        out = process.before.split("\r\n")
         return out
 
     def get_all_artists(self):
