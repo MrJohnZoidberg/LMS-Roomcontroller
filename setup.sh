663983a7a55e16641b8f12c87e7e2e02ce607c44
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-10-29 18:08:08 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Copy config.ini.default if config.ini doesn't exist.
-if [ ! -e config.toml ]; then
-    cp config.toml.setup config.toml
+DEFAULT_CONFIG_FILE="./config.toml.default"
+CONFIG_FILE="./config.toml"
+
+# user config version checking
+if [ ! -e $CONFIG_FILE ]; then
+    cp $DEFAULT_CONFIG_FILE $CONFIG_FILE
 else
-    user_line=$(sed '3q;d' config.toml)
-    user_version=${user_line//[!0-9]/}
-    setup_line=$(sed '3q;d' config.toml.setup)
-    setup_version=${setup_line//[!0-9]/}
-    if [ "$user_version" -lt "$setup_version" ]; then
-        cp config.toml config.toml.old
-        cp config.toml.setup config.toml
-        echo "The config.toml file has been updated and all values are reset to default."
+    user_ver=$(grep "config_ver" $CONFIG_FILE | sed 's/^config_ver=\([0-9]\.[0-9]\)/\1/g')
+    def_ver=$(grep "config_ver" $DEFAULT_CONFIG_FILE | sed 's/^config_ver=\([0-9]\.[0-9]\)/\1/g')
+
+    if [ "$def_ver" != "$user_ver" ]
+    then
+        echo "Current config options are overwrote by the new default value since they are out of date"
+        echo "The lastest config.ini version is $def_ver"
+        echo "Please change it manually to adapt to your old setup after installation"
         echo "For your previous values, look in config.toml.old"
+        cp $CONFIG_FILE "$CONFIG_FILE.old"
+        cp $DEFAULT_CONFIG_FILE $CONFIG_FILE
+    else
+        echo "Good config.ini version: $user_ver"
     fi
 fi
 
