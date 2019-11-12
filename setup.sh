075770724074d02b06d21e4b1e30a40161c76b2d
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-11-12 17:47:55 +0100

    Compile squeezelite source and add output colors

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# TODO: Color echo outputs
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEFAULT_CONFIG_FILE="./config.toml.default"
 CONFIG_FILE="./config.toml"
@@ -8,61 +7,65 @@ SQUEEZELITE_ENV_FILE=".squeezelite_env"
 CONTROLLER_LOG_FILE=".controller.log"
 PLAYER_MACS_FILE=".player_macs"
 
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
+
 # user config version checking
 if [ ! -e $CONFIG_FILE ]; then
     cp $DEFAULT_CONFIG_FILE $CONFIG_FILE
-    echo "A new config file was created from default config file: $CONFIG_FILE"
+    echo "${yellow}A new config file was created from default config file: $CONFIG_FILE"
     echo "Please open this file now and set the values according to your needs."
-    echo "After that, execute this setup file (./setup.sh) again."
-    exit 1
+    echo "After that, execute this setup file (./setup.sh) again.${reset}"
+    exit
 else
     user_ver=$(grep "config_ver" $CONFIG_FILE | sed 's/^config_ver=\([0-9]\.[0-9]\)/\1/g')
     def_ver=$(grep "config_ver" $DEFAULT_CONFIG_FILE | sed 's/^config_ver=\([0-9]\.[0-9]\)/\1/g')
-
-    if [ "$def_ver" != "$user_ver" ]
-    then
-        echo "Current config options are overwrote by the new default value since they are out of date"
-        echo "The lastest config.ini version is $def_ver"
-        echo "Please change it manually to adapt to your old setup after installation"
-        echo "For your previous values, look in config.toml.old"
+    if [ "$def_ver" != "$user_ver" ]; then
+        echo "${yellow}Current config options are overwrote by the new default value since they are out of date."
+        echo "Please open this file now and change it manually to adapt to your old setup after installation."
+        echo "For your previous values, look in config.toml.old${reset}"
         cp $CONFIG_FILE "$CONFIG_FILE.old"
         cp $DEFAULT_CONFIG_FILE $CONFIG_FILE
-    else
-        echo "Good config.ini version: $user_ver"
+        exit
     fi
 fi
 
 # install packages
-echo "Installing required packages... (libev4 libfaad2 liblirc-client0 libmad0 libuv1 libwebsockets8)"
-sudo apt autoremove -y squeezelite
+echo "${green}Installing required packages... (libev4 libfaad2 liblirc-client0 libmad0 libuv1 libwebsockets8)${reset}"
 sudo apt install -y libev4 libfaad2 liblirc-client0 libmad0 libuv1 libwebsockets8
-echo "Downloading squeezeplayer 1.9.6 from sourceforge (in Raspbian repos it is version 1.8, too much bugs)..."
-wget https://liquidtelecom.dl.sourceforge.net/project/lmsclients/squeezelite/linux/squeezelite-1.9.6.1198-armv6hf.tar.gz
-echo "Unpacking tar file..."
-tar -xvzf squeezelite-1.9.6.1198-armv6hf.tar.gz
-echo "Copying squeezelite binary to /usr/bin/squeezelite in system..."
+echo "${green}Removing old squeezelite package...${reset}"
+sudo apt autoremove -y squeezelite
+echo "${green}Downloading squeezelite source from github.com/MrJohnZoidberg...${reset}"
+git clone https://github.com/MrJohnZoidberg/squeezelite.git
+cd squeezelite || echo "${red}Failed to download squeezelite source. Exit.${reset}" && exit
+echo "${green}Installing build dependencies...${reset}"
+sudo apt install -y libasound2-dev libflac-dev libmad0-dev libvorbis-dev libmpg123-dev libfaad-dev
+echo "${green}Compiling binary file...${reset}"
+make
+echo "${green}Copying squeezelite binary to /usr/bin/squeezelite in system...${reset}"
 sudo cp ./squeezelite /usr/bin/squeezelite
-sudo chmod +x /usr/bin/squeezelite
-rm -rf LICENSE.txt LIBS.txt squeezelite-1.9.6.1198-armv6hf.tar.gz
+cd ..
+rm -rf squeezelite
 
 PYTHON=$(command -v python3)
 VENV=venv
 
-if [ -f "$PYTHON" ]
-then
-    if [ ! -d $VENV ]
-    then
-        # Create a virtual environment if it doesn't exist.
-        $PYTHON -m venv $VENV
-    fi
-    echo "Installing requirements with pip..."
-    # Activate the virtual environment and install requirements.
-    # shellcheck source=/dev/null
-    . $VENV/bin/activate
-    pip3 install -r requirements.txt
-else
-    echo "Cannot find Python 3. Please install it."
+if [ ! -f "$PYTHON" ]; then
+    echo "${red}Cannot find Python 3. Please install it. Exit.${reset}"
+    exit
 fi
+
+echo "${green}Installing requirements with pip...${reset}"
+if [ ! -d $VENV ]; then
+    # Create a virtual environment if it doesn't exist.
+    $PYTHON -m venv $VENV
+fi
+# Activate the virtual environment and install requirements.
+# shellcheck source=/dev/null
+. $VENV/bin/activate
+pip3 install -r requirements.txt
 
 LMS_SERVICE="
 [Unit]
@@ -86,8 +89,8 @@ Description=Squeezelite Player
 After=network.target sound.target
 
 [Service]
-Nice=-10
-LimitRTPRIO=98
+Nice=-20
+LimitRTPRIO=infinity
 LimitMEMLOCK=infinity
 PIDFile=/run/squeezelite.pid
 EnvironmentFile=-$DIR/$SQUEEZELITE_ENV_FILE
@@ -106,14 +109,14 @@ touch $CONTROLLER_LOG_FILE
 touch $PLAYER_MACS_FILE
 
 # create services
-echo "Creating systemd services (lms-roomcontroller.service and squeezelite-custom.service)..."
+echo "${green}Creating systemd services (lms-roomcontroller.service and squeezelite-custom.service)...${reset}"
 echo "$LMS_SERVICE" | sudo tee /lib/systemd/system/lms-roomcontroller.service >/dev/null
 echo "$SQUEEZELITE_SERVICE" | sudo tee /lib/systemd/system/squeezelite-custom.service >/dev/null
 sudo systemctl daemon-reload
 sudo systemctl kill -f squeezelite
 sudo systemctl disable squeezelite
-echo "Enabling lms-roomcontroller service..."
+echo "${green}Enabling lms-roomcontroller service...${reset}"
 sudo systemctl enable -f lms-roomcontroller
-echo "Starting lms-roomcontroller service..."
+echo "${green}Starting lms-roomcontroller service...${reset}"
 sudo systemctl restart lms-roomcontroller
-echo "Finished successfully."
+echo "${green}Finished successfully.${reset}"
